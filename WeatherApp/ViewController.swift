5fbcc1e0e4b5357a4d4755764fa19a0fc52fec5b
Aaron de Melo <aarondemelo@gmail.com>
2022-09-06 23:55:42 +0530

    sample app to study ios 12

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Aaron demelo on 03/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
+    }
+
+
+}
+
